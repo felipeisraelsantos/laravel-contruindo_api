2dcb2bc6285702da62cd160b511b2ba0a6df5ff8
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2022-12-05 22:45:25 -0300

    criando listener

---
+++ app/Listeners/EmailUsersAboutSeriesCreated.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\SeriesCreatedEvent;
+use App\Mail\SeriesCreated;
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+
+class EmailUsersAboutSeriesCreated
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(SeriesCreatedEvent $event)
+    {
+        $usuarioList = User::all();
+        foreach ($usuarioList as $index => $user) {
+            $email = new SeriesCreated(
+                $event->seriesName,
+                $event->seriesId,
+                $event->seriesSeasonsQty,
+                $event->seriesEpisodesPerSeason
+            );
+            $when = now()->addSecond($index * 2);
+            Mail::to($user)->later($when, $email);
+        }
+    }
+}
