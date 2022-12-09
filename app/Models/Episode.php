77f749f6cdbf868f653a3560a34ef92c69764ff5
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2022-12-09 00:14:15 -0300

    casting de campo

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,7 +9,7 @@ class Episode extends Model
 {
     use HasFactory;
     public $timestamps = false;
-    // protected $casts = ['watched' => 'boolean'];
+    protected $casts = ['watched' => 'boolean'];
     protected $fillabe = ['number'];
 
     public function season()
